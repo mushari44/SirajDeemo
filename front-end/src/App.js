826c890f54ade16d77e2d7d2fc966001cd6acf26
mushari44 <meeshoo-010@hotmail.com>
2024-12-24 11:19:51 +0300

    last

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./style.css";
+import logo from "./assets/logo-white.png";
+import Card from "./components/Card";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo bg-green" alt="logo" />
       </header>
+
+      <Card />
     </div>
   );
 }
---
+++ front-end/src/components/Card.jsx
@@ -0,0 +1,15 @@
+// Card.jsx
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="bg-white shadow-lg p-4 rounded-md">
+      <div>
+        
+      </div>
+      <input className="bg-gray-500"></input>
+    </div>
+  );
+};
+
+export default Card;
