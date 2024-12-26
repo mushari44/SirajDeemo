e6d495c1c5e96403e16564cf50a8742583d33aba
Abdullah1z <ahmabod506@gmail.com>
2024-12-26 10:09:13 +0300

    mergeA2

--- front-end/src/components/WelcomeMessage.jsx
+++ front-end/src/components/WelcomeMessage.jsx
@@ -4,7 +4,7 @@ import sirajIcon from "../assets/Siraj-icon.png";
 const WelcomeMessage = ({ timestamp }) => {
 
   const welcomingText = "WELCOME TEST MERGE ";
-  const suggestQ = "ما هي مواضيع المدونة؟";
+  const suggestQ = "ما هي hg المدونة؟";
 
   return (
     <div className="Message-Suggests">
